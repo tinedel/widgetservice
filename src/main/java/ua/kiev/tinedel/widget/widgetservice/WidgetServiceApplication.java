194d884b61d3c56f037c0ae367963354ef387a25
Ivan Volzhev <tinedel@tinedel.kiev.ua>
2020-09-27 16:14:42 +0200

    initial springboot + maven + maven wrapper commit

---
+++ src/main/java/ua/kiev/tinedel/widget/widgetservice/WidgetServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.kiev.tinedel.widget.widgetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/kiev/tinedel/widget/widgetservice/WidgetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kiev.tinedel.widget.widgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
